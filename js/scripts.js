333fc718b1c165b13240d10a247622578c552ef2
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2015-05-05 18:01:48 -0700

    add jquery transitions and responsive design

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,20 @@
 //   $('faded-glory').fadeTo(fast, 0.2).delay 300
 //   return
 
-$(document).ready(function() {
-	$("#faded-glory").delay(3000).fadeTo(2000, 0.33);
-});
+// $(document).ready(function() {
+// 	$("#faded-glory").delay(1900).fadeTo(2000, 0.33);
+// 	$("#text-1").delay(1000).fadeIn(1000);
+// 	$("#text-2").delay(3000).fadeIn(1000);
+// 	$("#text-3").delay(5200).fadeIn(1000);
+// 	$("#text-4").delay(7200).fadeIn(1000);
+// 	$("#text-5").delay(9300).fadeIn(1000);
+// 	$("#pulse").delay(9300).fadeTo(1000, .6);
+// 	$("#pulse").delay(1000).fadeTo(1000, 1.0);
+// 	$("#pulse").delay(1000).fadeTo(1000, .6);
+// 	$("#pulse").delay(1000).fadeTo(1000, 1.0);
+// 	$("#pulse").delay(1000).fadeTo(1000, .6);
+// 	$("#pulse").delay(1000).fadeTo(1000, 1.0);
+// });
 
 // $(document).ready(function() {
 //   $("h1").click(function() {
