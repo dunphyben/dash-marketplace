0aa51f2f463ede480f10b3ff5dfe8f8039353065
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2015-05-05 20:16:02 -0700

    fix form for mobile use. add footer + logo

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,38 @@
 //   $('faded-glory').fadeTo(fast, 0.2).delay 300
 //   return
 
-// $(document).ready(function() {
-// 	$("#faded-glory").delay(1900).fadeTo(2000, 0.33);
-// 	$("#text-1").delay(1000).fadeIn(1000);
-// 	$("#text-2").delay(3000).fadeIn(1000);
-// 	$("#text-3").delay(5200).fadeIn(1000);
-// 	$("#text-4").delay(7200).fadeIn(1000);
-// 	$("#text-5").delay(9300).fadeIn(1000);
-// 	$("#pulse").delay(9300).fadeTo(1000, .6);
-// 	$("#pulse").delay(1000).fadeTo(1000, 1.0);
-// 	$("#pulse").delay(1000).fadeTo(1000, .6);
-// 	$("#pulse").delay(1000).fadeTo(1000, 1.0);
-// 	$("#pulse").delay(1000).fadeTo(1000, .6);
-// 	$("#pulse").delay(1000).fadeTo(1000, 1.0);
-// });
+(function() {
+  $(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+      var $target, target;
+      e.preventDefault();
+      target = this.hash;
+      $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 900, 'swing', function() {
+        window.location.hash = target;
+      });
+    });
+  });
+}).call(this);
+
+$(document).ready(function() {
+	$("#faded-glory").delay(1900).fadeTo(2000, 0.33);
+	$("#text-1").delay(1000).fadeIn(1000);
+	$("#text-2").delay(3000).fadeIn(1000);
+	$("#text-3").delay(5200).fadeIn(1000);
+	$("#text-4").delay(7200).fadeIn(1000);
+	$("#text-5").delay(9300).fadeIn(1000);
+	$("#pulse").delay(9300).fadeTo(1000, .6);
+	$("#pulse").delay(1000).fadeTo(1000, 1.0);
+	$("#pulse").delay(1000).fadeTo(1000, .6);
+	$("#pulse").delay(1000).fadeTo(1000, 1.0);
+	$("#pulse").delay(1000).fadeTo(1000, .6);
+	$("#pulse").delay(1000).fadeTo(1000, 1.0);
+	$("#pulse").delay(1000).fadeTo(1000, .6);
+	$("#pulse").delay(1000).fadeTo(1000, 1.0);
+});
 
 // $(document).ready(function() {
 //   $("h1").click(function() {
